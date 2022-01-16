a8dc02db329e5496f85c25219c9873e4a64168d7
MphatsoMphepo <bsc-inf-20-19@unima.ac.mw>
2022-01-16 08:01:08 +0200

    Added swagger Documentation and working

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,16 +3,44 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 
 @SpringBootApplication
 
-public class DemoAppli {
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 
+	@Primary
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"My REST API",
+				"Some custom description of API.",
+				"API TOS",
+				"Terms of service",
+				new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
+				"License of API", "API license URL", Collections.emptyList());
+	}
 }
- 
--- src/main/java/com/example/demo/customer/CustomerConfig.java
+++ src/main/java/com/example/demo/customer/CustomerConfig.java
@@ -3,6 +3,11 @@ package com.example.demo.customer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -12,6 +17,7 @@ import static java.time.Month.*;
 
 @Configuration
 public class CustomerConfig {
+
 
     @Bean
     CommandLineRunner commandLineRunner(
@@ -34,4 +40,14 @@ public class CustomerConfig {
 
         };
     }
+
+//    @Primary
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
 }
